364fe9093145fb2df33c5fddd0822aeecf52d803
longsutls <longsutls@gmail.com>
2018-08-08 20:33:43 +0700

    Gán giá trị min và so sánh. Sau đó in ra kết quả. Done

--- src/MinValue.java
+++ src/MinValue.java
@@ -14,6 +14,17 @@ public class MinValue {
         } while (size > 10);
         array = new int[size];
 
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Nhập vào giá trị cho em: ");
+            array[i] = sc.nextInt();
+        }
 
+        int min = array[0];
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] < min)
+                min = array[j];
+        }
+
+        System.out.println("Giá trị nhỏ nhất là: " + min);
     }
 }
