c1b2e03291719e23e7f7858c6b925b2b7814f079
longsutls <longsutls@gmail.com>
2018-08-08 20:16:39 +0700

    Khai báo mảng và yêu cầu nhập kích thước cho mảng. tối đa là 10

--- src/MinValue.java
+++ src/MinValue.java
@@ -5,6 +5,15 @@ public class MinValue {
         Scanner sc = new Scanner(System.in);
 
         int size;
+        int[] array;
+        do {
+            System.out.print("Nhập vào kích thước của mảng: ");
+            size = sc.nextInt();
+            if (size > 10)
+                System.out.println("Kích thước nhỏ hơn 10 thôi nha!");
+        } while (size > 10);
+        array = new int[size];
+
 
     }
 }
